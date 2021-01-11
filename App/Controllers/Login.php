125533852272e3d9859a58fcdb6627ebba552ddd
justynalezuch <justynalezuch@gmail.com>
2021-01-11 11:13:51 +0100

    Add composer autoloader

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace App\Controllers;
+use Core\View;
 
 class Login extends \Core\Controller
 {
 
     public function indexAction(){
-        
+
         echo htmlspecialchars(print_r($this->route_params, true));
     }
+
+    public function newAction(){
+
+        View::renderTemplate('Login/new.html');
+    }
+
+
 
 }
