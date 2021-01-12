cf2f736f716f96b5c91068b8d90e5f019d30531c
justynalezuch <justynalezuch@gmail.com>
2021-01-12 18:35:25 +0100

    Set http response code

--- Core/Error.php
+++ Core/Error.php
@@ -40,6 +40,8 @@ class Error
             $code = 500;
         }
 
+        http_response_code($code);
+
         if(\App\Config::SHOW_ERRORS) {
             echo "<h1>Fatal error</h1>";
             echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
