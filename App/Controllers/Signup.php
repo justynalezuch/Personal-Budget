358f6a9f00e4ba6513a4044712b2782b275fb909
justynalezuch <justynalezuch@gmail.com>
2021-01-13 18:32:24 +0100

    Validation - server side

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -4,12 +4,37 @@
 namespace App\Controllers;
 
 
+use App\Models\User;
 use Core\View;
 
 class Signup extends \Core\Controller
 {
+    /**
+     * Singup form view
+     */
     public function newAction() {
 
         View::renderTemplate('Signup/new.html');
     }
+
+
+    /**
+     * Sign up new user
+     *
+     */
+    public function createAction()
+    {
+        $user = new User($_POST);
+
+        if($user->save()) {
+
+            View::renderTemplate('Signup/success.html');
+
+        } else {
+            var_dump($user->errors);
+//            Vie
+
+        }
+    }
+
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -7,5 +7,100 @@ use PDO;
 
 class User extends \Core\Model
 {
+    /**
+     * Error messages
+     * @var array
+     */
+    public $errors = [];
+
+
+    public function __construct($data = []) {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function save() {
+
+        $this->validate();
+
+        if(empty($this->errors)) {
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (name, email, password_hash)
+            VALUES (:name, :email, :password_hash)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute(); // return true or false
+        }
+        return false;
+    }
+    public function validate() {
+
+        // Name
+        if ($this->name == '') {
+            $this->errors[] = 'Podaj nazwę użytkownika';
+        }
+
+        if (ctype_alnum($this->name) == false) {
+            $this->errors[] = "Nazwa użytkownika może składać się tylko z liter i cyfr (bez polskich znaków)";
+        }
+
+        if (strlen($this->name) < 3 || strlen($this->name) > 50) {
+            $this->errors[] = "Nazwa użytkownika powinna być nie krótsza niż 3 znaki i nie dłuższa niż 50 znaków.";
+        }
+
+        // Email address
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+            $this->errors[] = 'Podaj poprawny adres email.';
+        }
+
+        if(static::emailExists($this->email)) {
+            $this->errors[] = 'Istnieje już konto zarejestrowane na podany adres email.';
+        }
+
+        if (strlen($this->email) > 50) {
+            $this->errors[] = "Adres email nie może być dłuższy niż 50 znaków.";
+        }
+
+        // Password
+        if (strlen($this->password) < 8 || strlen($this->password) > 20) {
+            $this->errors[] = "Hasło musi posiadać od 8 do 20 znaków.";
+        }
+
+        if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Hasło musi posiadać co najmniej jedną literę.';
+        }
+
+        if (preg_match('/.*\d+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Hasło musi posiadać co najmniej jedną cyfrę.';
+        }
+    }
+
+
+    /**
+     * Check if email already exist.
+     *
+     * @param $email
+     * @return bool
+     */
+    public static function emailExists($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch() !== false;
+    }
 
 }
