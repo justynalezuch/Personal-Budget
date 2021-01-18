1871714ca4d28ea551aca5d3f19b98e3702918dc
justynalezuch <justynalezuch@gmail.com>
2021-01-18 17:55:08 +0100

    Login view

--- public/index.php
+++ public/index.php
@@ -22,5 +22,6 @@ $router = new Core\Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
+$router->add('login', ['controller' => 'Login', 'action' => 'new']);
 
 $router->dispatch($_SERVER['QUERY_STRING']);
