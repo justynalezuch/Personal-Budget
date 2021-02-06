c2e5f109dd8b61639fbb9889c76de32c9ef5193b
justynalezuch <justynalezuch@gmail.com>
2021-02-06 12:39:03 +0100

    Setting view - tabs

---
+++ App/Controllers/Settings.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use Core\View;
+
+class Settings extends Authenticated
+{
+    public function indexAction() {
+        View::renderTemplate('Settings/index.html');
+    }
+
+}
