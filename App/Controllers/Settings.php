bcd41a227ea91e39a507e71a90470c7652010109
justynalezuch <justynalezuch@gmail.com>
2021-02-06 14:06:18 +0100

    User data edit form - view

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -3,13 +3,21 @@
 
 namespace App\Controllers;
 
-
+use App\Auth;
 use Core\View;
 
 class Settings extends Authenticated
 {
     public function indexAction() {
-        View::renderTemplate('Settings/index.html');
+        View::renderTemplate('Settings/index.html', [
+            'user' => Auth::getUser()
+        ]);
     }
+
+    public function userUpdateAction() {
+        echo 'update';
+    }
+
+
 
 }
